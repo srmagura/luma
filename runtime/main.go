46f5642fc44fb813ce1184f97081f4f008e7ea10
Sam Magura <srmagura@gmail.com>
2026-03-09 09:46:48 -0400

    evaulate ast

--- runtime/main.go
+++ runtime/main.go
@@ -1,10 +1,26 @@
 package runtime
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/srmagura/luma/shared"
 )
 
-func Execute(ast shared.Node) {
-	shared.PrintAST(ast)
+type Node = shared.Node
+type IntLiteral = shared.IntLiteral
 
+func Execute(ast Node) {
+	i := eval(ast)
+	fmt.Println(i)
+}
+
+func eval(n Node) int {
+	switch v := n.(type) {
+	case IntLiteral:
+		return v.Value
+	default:
+		log.Fatal("Unexpected node:", v)
+		return 0
+	}
 }
