23499a37a4fd875bb31a6cf6ff3351914a430c17
Sam Magura <srmagura@gmail.com>
2026-03-14 09:22:41 -0400

    About to remove runtime_test

--- runtime/runtime.go
+++ runtime/runtime.go
@@ -4,38 +4,93 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"strconv"
+	"strings"
 
 	"github.com/srmagura/luma/shared"
 )
 
-func Execute(ast shared.Node, out io.Writer) {
-	i := evalExpr(ast)
-	fmt.Fprintln(out, i)
+type env struct {
+	out io.Writer
 }
 
-func evalExpr(n shared.Node) int {
+func Execute(ast shared.Node, out io.Writer) {
+	env := &env{out: out}
+	env.evalExpr(ast)
+}
+
+func (env *env) evalExpr(n shared.Node) int {
 	switch v := n.(type) {
 	case shared.IntLiteral:
 		return v.Value
 	case shared.BinaryExpr:
-		return evalBinaryExpr(v)
+		return env.evalBinaryExpr(v)
+	case shared.CallExpr:
+		return env.evalCallExpr(v)
 	default:
 		log.Fatal("eval: Unexpected node type: ", n)
 		return 0
 	}
 }
 
-func evalBinaryExpr(n shared.BinaryExpr) int {
-	left := evalExpr(n.Left)
-	right := evalExpr(n.Right)
+func (env *env) evalBinaryExpr(n shared.BinaryExpr) int {
+	left := env.evalExpr(n.Left)
+	right := env.evalExpr(n.Right)
 
 	switch n.Op {
 	case shared.OpAdd:
 		return left + right
 	case shared.OpSubtract:
 		return left - right
+	case shared.OpMultiply:
+		return left * right
+		// TODO implement
+		//case shared.OpDivide:
+		//	return left / right
+	case shared.OpDivideInteger:
+		return left / right
 	default:
+		// TODO error handling
 		log.Fatal("Unexpected binary operator: ", n.Op)
 		return 0
 	}
+}
+
+func (env *env) evalCallExpr(n shared.CallExpr) int {
+	args := make([]int, len(n.Args))
+
+	for i, v := range n.Args {
+		args[i] = env.evalExpr(v)
+	}
+
+	switch v := n.Func.(type) {
+	case shared.IdentNode:
+		if v.Name == "print" {
+			return env.evalPrint(args)
+		}
+
+		// TODO allow other functions
+		log.Fatal("print is the only valid function currently")
+		return 0
+	default:
+		// TODO error handling
+		log.Fatal("Only identifiers are valid as a function")
+		return 0
+	}
+}
+
+func (env *env) evalPrint(args []int) int {
+	var sb strings.Builder
+
+	for i, arg := range args {
+		sb.WriteString(strconv.Itoa(arg))
+		if i != len(args)-1 {
+			sb.WriteString(" ")
+		}
+	}
+
+	fmt.Fprintln(env.out, sb.String())
+
+	// TODO print should not return anything
+	return 0
 }
--- runtime/runtime_test.go
+++ runtime/runtime_test.go
@@ -1,23 +1,27 @@
 package runtime
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/srmagura/luma/compiler"
 )
 
-func testEvalExpr(t *testing.T, src string, expected int) {
+func testExecute(t *testing.T, src string, expected string) {
 	ast, err := compiler.Compile(src)
 	if err != nil {
 		t.Fatal(err.Error())
 	}
 
-	result := evalExpr(ast)
+	var sb strings.Builder
+	Execute(ast, &sb)
+	result := sb.String()
+
 	if result != expected {
-		t.Fatalf("Result was: %d", result)
+		t.Fatalf("Result was: %s", result)
 	}
 }
 
 func TestAddition(t *testing.T) {
-	testEvalExpr(t, "2 - 3 + 4", 3)
+	testExecute(t, "2 - 3 + 4", "3")
 }
